74dcb3550703d7e43336dd40d08afb452a5187c9
Kybox <nslr@riseup.net>
2020-01-03 23:06:40 +0100

    Adding ModelMapper feature

--- src/main/java/fr/kybox/config/AppConfig.java
+++ src/main/java/fr/kybox/config/AppConfig.java
@@ -1,6 +1,7 @@
 package fr.kybox.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +11,10 @@ public class AppConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
--- src/main/java/fr/kybox/service/impl/Mp3ServiceImpl.java
+++ src/main/java/fr/kybox/service/impl/Mp3ServiceImpl.java
@@ -5,20 +5,22 @@ import com.mpatric.mp3agic.*;
 import fr.kybox.entity.Mp3Data;
 import fr.kybox.service.Mp3Service;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 
-@Slf4j
 @Service
 public class Mp3ServiceImpl implements Mp3Service {
 
+    private final ModelMapper modelMapper;
     private final ObjectMapper objectMapper;
 
-    public Mp3ServiceImpl(ObjectMapper objectMapper) {
+    public Mp3ServiceImpl(ObjectMapper objectMapper, ModelMapper modelMapper) {
         this.objectMapper = objectMapper;
+        this.modelMapper = modelMapper;
     }
 
     public Mp3File load() throws InvalidDataException, IOException, UnsupportedTagException {
@@ -33,15 +35,7 @@ public class Mp3ServiceImpl implements Mp3Service {
             ID3v2 mp3Tags = mp3File.getId3v2Tag();
             Mp3Data mp3Data = new Mp3Data();
 
-            mp3Data.setAlbum(mp3Tags.getAlbum());
-            mp3Data.setArtist(mp3Tags.getArtist());
-            mp3Data.setTitle(mp3Tags.getTitle());
-            mp3Data.setComposer(mp3Tags.getComposer());
-            mp3Data.setGenre(mp3Tags.getGenre());
-            mp3Data.setGenreDesc(mp3Tags.getGenreDescription());
-            mp3Data.setTrack(mp3Tags.getTrack());
-            mp3Data.setYear(mp3Tags.getYear());
-            mp3Data.setCover(mp3Tags.getAlbumImage());
+            this.modelMapper.map(mp3Tags, mp3Data);
 
             return mp3Data;
 
@@ -64,17 +58,12 @@ public class Mp3ServiceImpl implements Mp3Service {
         Mp3File mp3File = this.load();
         ID3v2 mp3Tags = mp3File.getId3v2Tag();
 
-        mp3Tags.setAlbum(mp3Data.getAlbum());
-        mp3Tags.setTrack(mp3Data.getTrack());
-        mp3Tags.setTitle(mp3Data.getTitle());
-        mp3Tags.setArtist(mp3Data.getArtist());
-        mp3Tags.setComposer(mp3Data.getComposer());
-        mp3Tags.setYear(mp3Data.getYear());
-        mp3Tags.setGenre(mp3Data.getGenre());
-        mp3Tags.setCopyright("yann@kybox.fr");
+        this.modelMapper.map(mp3Data, mp3Tags);
+
+        mp3Tags.setCopyright("Open source");
         mp3Tags.setEncoder("Mp3agic");
         mp3Tags.setUrl("https://kybox.fr");
-        if(file != null)
+        if (file != null)
             mp3Tags.setAlbumImage(file.getBytes(), "Cover");
 
         mp3File.save("./Comp7-updated.mp3");
